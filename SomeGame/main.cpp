1bd5892ba32a76784f8a2f431b5fd7632eefdf25
Dylan Socolobsky <dsocolobsky@gmail.com>
2015-01-12 21:02:27 -0300

    Go back to when it worked

--- SomeGame/main.cpp
+++ SomeGame/main.cpp
@@ -3,9 +3,11 @@
 #include "Game.h"
 
 int main(int argc, char *argv[]) {
-	std::unique_ptr<Game> game(new Game(800, 600, "SFML Game"));
+	std::unique_ptr<Game> game(new Game);
+	game->window.create(sf::VideoMode(800, 600), "SFML Game");
+	std::unique_ptr<Screen> screen(&game->menuScreen);
 
 	game->run();
-	
+
 	return 0;
 }
