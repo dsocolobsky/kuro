6c1fc6da2e39a420f5e52a1283c78a343f913c1b
Dylan Socolobsky <dsocolobsky@gmail.com>
2015-01-12 21:03:21 -0300

    Revert "Actually start on MenuScreen"
    This reverts commit cd5bcc391c33dc59c6354623313b0a69d7929ccf.

--- SomeGame/main.cpp
+++ SomeGame/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
 	std::unique_ptr<Game> game(new Game);
 	game->window.create(sf::VideoMode(800, 600), "SFML Game");
-	std::unique_ptr<Screen> screen(&game->menuScreen);
+	std::unique_ptr<Screen> screen(&game->gameScreen);
 
 	game->run();
 
